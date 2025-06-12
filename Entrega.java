81766b9e18d51be4adba344241340d58e1fd1608
toperwoper <82802783+toperwoper@users.noreply.github.com>
2025-06-12 17:38:42 +0100

    Update Entrega.java
    Daba error si los nombres no estaban entre comillas

--- Entrega.java
+++ Entrega.java
@@ -49,7 +49,7 @@ import java.util.stream.IntStream;
  * de texte estigui configurat amb codificació UTF-8.
  */
 class Entrega {
-  static final String[] NOMS = {Andreu Morey Juan, Miquel Matas Soler, Antonio José Pérez García};
+  static final String[] NOMS = {"Andreu Morey Juan", "Miquel Matas Soler", "Antonio José Pérez García"};
 
   /*
    * Aquí teniu els exercicis del Tema 1 (Lògica).
